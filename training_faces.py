81f9fc352bf639136c3e5a01c213c27bdcac7661
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-04-01 23:36:24 +0800

    updated

--- training_faces.py
+++ training_faces.py
@@ -2,4 +2,11 @@ import os as Machine
 
 BASE_DIRECTORY = Machine.path.dirname(Machine.path.abspath(__file__))
 IMAGE_DATABASE_DIRECTORY = Machine.path.join(BASE_DIRECTORY, "model")
-print(BASE_DIRECTORY, IMAGE_DATABASE_DIRECTORY)
+# print(IMAGE_DATABASE_DIRECTORY)
+
+for root, dirs, files in Machine.walk(IMAGE_DATABASE_DIRECTORY):
+      for file in files:
+            if file.endswith("jpg") or file.endswith("png"): # Prefered (.png)
+                  path = Machine.path.join(root, file)
+                  label = Machine.path.basename(Machine.path.dirname(path)).replace(" ","-").upper()
+                  print(label, path)
