bdd8eda08aa420a2dc96263d0b209b834df143db
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-04-02 14:41:51 +0800

    finalised<training>

--- training_faces.py
+++ training_faces.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#              Copyright 2020 © John Melody Melissa
+#              Copyright 2020 © John Melody Me
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# @Author : John Melody Melissa
-# @Copyright: John Melody Melissa & Tan Sin Dee © Copyright 2020
+# @Author : John Melody Me
+# @Copyright: John Melody Me & Tan Sin Dee © Copyright 2020
 # @INPIREDBYGF: Cindy Tan Sin Dee <3
 # @Project: FacialRecognition.py
 import cv2
