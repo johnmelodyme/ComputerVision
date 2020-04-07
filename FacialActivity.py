49e336ca207046a2a11553a1c4a5cb49b3d4f69c
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-04-07 23:24:40 +0800

    updated

--- FacialActivity.py
+++ FacialActivity.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
-#              Copyright 2020 © John Melody Me
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#             http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# @Author : John Melody Me
-# @Copyright: John Melody Me & Tan Sin Dee © Copyright 2020
-# @INPIREDBYGF: Cindy Tan Sin Dee <3
-# @Project: FacialRecognition.py
+Copyright = """
+             Copyright 2020 © John Melody Me
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+@Author : John Melody Me
+@Copyright: John Melody Me & Tan Sin Dee © Copyright 2020
+@INPIREDBYGF: Cindy Tan Sin Dee <3
+@Project: FacialRecognition.py
+"""
 import cv2
 from datetime import datetime, date
 import numpy as np
@@ -34,8 +36,8 @@ with open("labels.pickle", "rb") as file:
       labels = {v:k for k, v in old_labels.items()}
 # CV2 Config:
 capture = cv2.VideoCapture(0)
-print(capture)
 print(capture.read())
+print(Copyright)
 title = "Facial Recognition"
 # Machine.system("python training_faces.py") # Uncomment this if error
 #Capture Frame by Frame
@@ -51,7 +53,7 @@ while (True):
             # Prediction:
             id_ , conf = recognizer.predict(REGION_OF_INTEREST_GREY)
             if conf >= 45 and conf <= 85:
-                  print(id_)
+                  # print(id_)
                   print("Detected identity" ,labels[id_], "Accuracy: ", random.randint(1, 99), "%")
                   # OPENCV PUT TEXT:
                   font = cv2.FONT_HERSHEY_COMPLEX
